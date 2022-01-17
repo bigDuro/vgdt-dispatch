273df7d1a7104fa287c8e6dc421a1ca540c4b81d
bigDuro <darrylcrockett@gmail.com>
2022-01-17 13:31:41 -0500

    updated package

--- utilities/ZipFolder.js
+++ utilities/ZipFolder.js
@@ -4,7 +4,7 @@ const vgdtAPI_zip = 'build/public/api.zip';
 
 const codeigniter4_zip = 'build/non_public/codeigniter4/codeigniter4-beta.zip';
 const publicHtml = 'build/public/vgdt-dispatch';
-const publicHtml_zip = 'build/public/vgdt-dispatch-beta-v1.zip';
+const publicHtml_zip = 'build/public/vgdt-dispatch-beta-v2.zip';
 const publicAssets = 'src/assets';
 const publicAssets_zip = 'build/public/assets.zip';
 const publicAPI = 'build/public/api';
